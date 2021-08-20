b8e4a53c2006c61a5e6c9796551f8e0ef44871c1
Johannes von Foerster <jvonfoerster@gmail.com>
2021-08-20 14:03:19 +0100

    so far

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,12 +6,33 @@ class ListsController < ApplicationController
   end
 
   def show
-    @bookmark = Bookmark.new
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @list.destroy
+    redirect_to lists_path
   end
 
   private
 
   def set_list
     @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name, :photo)
   end
 end
